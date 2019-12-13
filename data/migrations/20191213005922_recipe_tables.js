81a51c07f46a92ea373d2cdf4542f396f86d1dbd
Rasha Moumneh <rasha@fastmail.com>
2019-12-13 01:15:24 -0500

    Add migrations

---
+++ data/migrations/20191213005922_recipe_tables.js
@@ -0,0 +1,51 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl
+        .text("recipe_name", 128)
+        .unique()
+        .notNullable();
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .text("ingredient_name", 128)
+        .unique()
+        .notNullable();
+    })
+    .createTable("recipe_insructions", tbl => {
+      tbl.increments();
+      tbl.text("step_number").notNullable();
+      tbl.text("step_description").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("recipe_ingredients", tbl => {
+      tbl.primary(["recipe_id", "ingredient_id"]);
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients");
+      tbl
+        .integer("quantity")
+        .unsigned()
+        .notNullable();
+    });
+};
+
+exports.down = function(knex) {};
